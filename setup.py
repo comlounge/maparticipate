8ae503cc8e5fcaed6454fe8869c7dfa2b0e2bc52
Christian Scholz <cs@comlounge.net>
2016-02-04 16:58:10 +0100

    added some readme stuff and classifiers

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ import sys, os
 
 version = '1.0'
 
+"""
+MaParticipate
+-------------
+
+MaParticipate is a tool capable of publically planning routes on a map. It can be used e.g.
+in a public process (which it was developed for originally). Users are able to create and 
+comment route proposals on different sub sections of a bigger route. 
+
+"""
+
 setup(name='maparticipate',
       version=version,
       description="Collaborative track proposal tool for eParticipation projects",
-      long_description="""
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=__doc__,
       keywords='',
       author='COM.lounge',
       author_email='info@comlounge.net',
@@ -16,6 +24,7 @@ setup(name='maparticipate',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
+      platforms='any',
       zip_safe=False,
       install_requires=[
         "starflyer",
@@ -32,8 +41,19 @@ setup(name='maparticipate',
         "setuptools",
         "requests",
       ],
-      entry_points="""
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+      ],
+      entry_points='''
           [paste.app_factory]
           main = maparticipate.app:app
-      """,
+      ''',
       )
